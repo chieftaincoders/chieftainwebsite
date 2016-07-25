c31e938eb319ac338ccc8b99fff8ae1dbfa8cae9
ccoder20 <court.hemmings.penguins@gmail.com>
2016-07-25 10:59:04 -0600

    Merge branch 'master' of C:\xampp\htdocs\chieftainwebsite with conflicts.

--- contact.php
+++ contact.php
@@ -24,29 +24,17 @@
     <br>
     Fax: (505) 368-5796
     <br>
-    Email:shs@centralschools.org
+    Email: shs@centralschools.org
 </p>
+
     <hr>
-</div>
+    
     <div class="container">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25565.311609326513!2d-108.69952016348522!
-    3d36.77862839392055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5ba10095167afc08!
-    2sShiprock+High+School!5e0!3m2!1sen!2sus!4v1469128114081"
-            width="600"
-            height="450"
-            frameborder="10"
-            style="border:0"
-            allowfullscreen></iframe>
-     </div>
-
-    <hr>
-<div class="container">
-    <form>
-
-            <legend> Message us:</legend>
+        <form class="forp">
+            <h3 class="forp"> Message us:</h3>
             <div class="form-group">
                 <label for="exampleInputName1">Name (required)</label>
-                 <input type="text"  class="form-control" id="exampleInputName1" placeholder="Marcel Stylinson">
+                <input type="text"  class="form-control" id="exampleInputName1" placeholder="Marcel Stylinson">
             </div>
 
             <div class="form-group">
@@ -58,8 +46,19 @@
                 <input type="text" class="form-control" placeholder="Text input">
             </div>
             <button type="submit" class="btn btn-default">Submit</button>
-
-    </form>
+        </form>
+    </div>
+    
 </div>
+    <div class="container">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25565.311609326513!2d-108.69952016348522!
+    3d36.77862839392055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5ba10095167afc08!
+    2sShiprock+High+School!5e0!3m2!1sen!2sus!4v1469128114081"
+            width="600"
+            height="450"
+            frameborder="10"
+            style="border:0"
+            allowfullscreen></iframe>
+     </div>
 </body>
 </html>
