08029166c6e95b8b1d13042c4cd6b39e488c801a
cc17 <alan.taliman1999@gmail.com>
2016-07-25 10:52:33 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php

--- about.php
+++ about.php
@@ -13,8 +13,29 @@
 
 <div class="mainImage-1" datasrc="...">
     <h1>About</h1>
-    <p><a class="btn btn-primary btn-large" href="#">Learn More</a></p>
 </div>
+
+<div class="divide">
+    <p> We are learning how to code and decode the information given to us by our teachers Charles
+        Krieg, Eric, and John. They taught us how to learn the keys and code given to us, we took these
+        codes and written them in our projects and files. They are great teachers, they taught us what being a coder
+        meant to us and with us writting codes for the websites. are the coders of shiprock High School
+        <img src=""</p>
+</div>
+
+<div>
+    <div>
+        <li>
+            <ul>
+                <h1>
+                    <p> Hello how are you ? </p>
+                </h1>
+            </ul>
+        </li>
+    </div>
+</div>
+
+<p>This is some text. <img src="" alt="Smiley face" align="middle"> This is some text.</p>
 
 </body>
 </html>
