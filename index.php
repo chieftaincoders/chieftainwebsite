4d9b1134babfbb4291ea40100a048a8286fa32e7
ccoder19 <osheagoodman@gmail.com>
2016-07-26 09:32:58 -0600

    fixed text borders showing by removing hover in the style.css

--- index.php
+++ index.php
@@ -6,21 +6,24 @@
 </head>
 <body>
 
+
+<div class="mainImage">
     <?php require_once("navbar.php");?>
-
-
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img class="first-slide"
-                 src="http://tinyurl.com/zrpw9oc"
-                 alt="firstSlide">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1 class="leadtag-carousel"> Chieftain Coders </h1>
-                </div>
-            </div>
-        </div>
 </div>
 
-</body>`
+
+<div class="carousel-inner" role="listbox">
+    <div class="item active">
+        <img class="first-slide"
+             src="http://tinyurl.com/zrpw9oc"
+             alt="firstSlide">
+        <div class="container">
+            <div class="carousel-caption">
+                <h1 class="leadtag-carousel"> Chieftain Coders </h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
 </html>
