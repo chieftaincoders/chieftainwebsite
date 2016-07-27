6d5935945ebdb64359908a6836a079f73319cd8d
ccoder20 <court.hemmings.penguins@gmail.com>
2016-07-27 12:22:57 -0600

    fixed the main image on contact page

--- contact.php
+++ contact.php
@@ -5,56 +5,51 @@
     <?php require_once("lib/head-utils.php");?>
 
 </head>
-<body class="bruh">
+<body>
 
-    <div class="mainImage-2">
-        <h1>Contact</h1>
-        <?php require_once("navbar.php");?>
+<?php require_once("navbar.php");?>
 
-    </div>
 
-<div class="container">
-    <h4>Chieftain Coders</h4>
-<p class="para">
-    <br>
-    Tel: (505) 368-5161
-    <br>
-    Fax: (505) 368-5796
-    <br>
-    Email: shs@centralschools.org
-</p>
-    
-    <br>
-    <div class="container">
-        <h3 class="forp"> Message us:</h3>
-        <form class="forp">
-            <div class="form-group">
-                <label for="exampleInputName1">Name (required)</label>
-                <input type="text"  class="form-control" id="exampleInputName1" placeholder="Marcel Stylinson">
-            </div>
-
-            <div class="form-group">
-                <label for="exampleInputEmail1">Your Email (required)</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputsubject1">Message</label>
-                <input type="text" class="form-control" placeholder="Subject">
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-    </div>
-    
+<div class="mainImage-2" datasrc="lib/images/Starry-Night.jpg">
+    <h1>Contact</h1>
 </div>
+
+<br>
+
+<div class="Bloop">
+
     <div class="container">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25565.311609326513!2d-108.69952016348522!
-    3d36.77862839392055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5ba10095167afc08!
-    2sShiprock+High+School!5e0!3m2!1sen!2sus!4v1469128114081"
-            width="600"
-            height="450"
-            frameborder="10"
-            allowfullscreen></iframe>
-     </div>
+        <h4>Chieftain Coders</h4>
+        <p class="para">
+            <br>
+            Tel: (505) 368-5161
+            <br>
+            Fax: (505) 368-5796
+            <br>
+            Email: shs@centralschools.org
+        </p>
+
+        <br>
+        <div class="container">
+            <h3 class="forp"> Message us:</h3>
+            <form class="forp">
+                <div class="form-group">
+                    <label for="exampleInputName1">Name (required)</label>
+                    <input type="text"  class="form-control" id="exampleInputName1" placeholder="Marcel Stylinson">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Your Email (required)</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputsubject1">Message</label>
+                    <input type="text" class="form-control" placeholder="Subject">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+</div>
 </body>
 
 <script src="lib/js/script.js"></script>
