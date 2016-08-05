ff16a8f63ad2733a30c0ed9ef60e3e8741e4e80d
ccoder20 <court.hemmings.penguins@gmail.com>
2016-08-05 09:53:54 -0600

    fixed the contact page

--- contact.php
+++ contact.php
@@ -14,9 +14,6 @@
     <h1>Contact</h1>
 </div>
 
-<br>
-
-<div class="Bloop container">
 
     <div class="container">
         <h4>Chieftain Coders</h4>
@@ -30,8 +27,6 @@
         </p>
     </div>
 
-</div>
-
 <div class="container">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25565.311609326513!2d-108.69952016348522!
     3d36.77862839392055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5ba10095167afc08!
@@ -44,6 +39,7 @@
 </body>
 
 <script src="lib/js/script.js"></script>
+
 <?php require_once("stickyfoot.php");?>
 
 
