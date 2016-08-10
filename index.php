8038942845a43b9c0f5927c5b9c48dfd1e90c244
cc17 <alan.taliman1999@gmail.com>
2016-08-10 11:31:13 -0600

    Added to portfolio in index.php

--- index.php
+++ index.php
@@ -65,6 +65,178 @@
                     <h4>Courtney Elwood  <a href="http://robinelwood.com"></h4>
                 </ul>
             </div>
+
+
+            <div id="floatright" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="http://tinyurl.com/h4mkoaz" class="img-circle">
+                <p id="codingCoders"><strong> Amber Henderson </strong>
+                    <br>
+                <p>
+                    <br>
+                    <i class="em em-panda_face"></i>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-panda_face"></i>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-panda_face"></i>
+                </p>
+                <a id="codersWeb" href="http://amoshma.com/">
+                    <button type="button" class="myButton">
+                        Amber's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+            <div id="floatleft" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="lib/pictures/alan.png" class="img-circle">
+                <p id="codingCoders"><strong> Alan Taliman </strong>
+                    <br>
+                <p> Hi my name is Alan Taliman, I'm gonna be a junnior this year 2016-2017, graduating in 2018 as
+                    a senior. derp
+                    <br>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-smile"></i>
+                    <i class="em em-arrow_down"></i>
+                </p>
+                <a id="codersWeb" href="http://allietech.com/">
+                    <button type="button" class="myButton">
+                        Alan's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+            <div id="floatleft" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="lib/pictures/rius.jpg" class="img-circle">
+                <p id="codingCoders"><strong> Rius Goodman </strong>
+                    <br>
+                <p>
+                    <br>
+                    <i class="em em-video_game"></i>
+                    <i class="em em-space_invader"></i>
+                    <i class="em em-video_game"></i>
+                </p>
+                <a id="codersWeb" href="http://riusbearjew.com/">
+                    <button type="button" class="myButton">
+                        Rius's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+            <div id="floatright" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="http://tinyurl.com/hatgfm6" class="img-circle">
+                <p id="codingCoders"><strong> Kea Chico </strong>
+                    <br>
+                <p>
+                    <br>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-pizza"></i>
+                    <i class="em em-arrow_down"></i>
+                </p>
+                <a id="codersWeb" href="http://keasioux.com">
+                    <button type="button" class="myButton">
+                        Kea's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+            <div id="floatright" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="http://tinyurl.com/hatgfm6" class="img-circle">
+                <p id="codingCoders"><strong> Marissa Shorty </strong>
+                    <br>
+                <p>
+                    <br>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-smiley_cat"></i>
+                    <i class="em em-arrow_down"></i>
+                </p>
+                <a id="codersWeb" href="http://marissashorty.com/#landHere">
+                    <button type="button" class="myButton">
+                        Marissa's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+            <div id="floatleft" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="lib/pictures/catThreeRed.JPG" class="img-circle">
+                <!--                http://tinyurl.com/hdjrpht-->
+                <p id="codingCoders"><strong> O'Shea Goodman </strong>
+                    <br>
+                <p> Hello my name is O'Shea Goodman i am going to be a Junior at Shiprock High School during the
+                    years of 2016-2017 i will be graduating with the class of 2018. My personal site is below just
+                    click the button to learn more about me.
+                    <br>
+                    <i class="em em-point_down"></i>
+                    <i class="em em-hatching_chick"></i>
+                    <i class="em em-point_down"></i>
+                </p>
+                <a id="codersWeb" href="http://osheadev.com/">
+                    <button type="button" class="myButton">
+                        O'Shea's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+            <div id="floatright" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="http://tinyurl.com/hatgfm6" class="img-circle">
+                <p id="codingCoders"><strong> Courtney Elwood </strong>
+                    <br>
+                <p>
+                    <br>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-pizza"></i>
+                    <i class="em em-arrow_down"></i>
+                </p>
+                <a id="codersWeb" href="http://robinelwood.com/">
+                    <button type="button" class="myButton">
+                        Courtney's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+
+
+            <div id="floatright" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="http://tinyurl.com/hatgfm6" class="img-circle">
+                <p id="codingCoders"><strong> Adam Pettigrew </strong>
+                    <br>
+                <p>
+                    <br>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-pizza"></i>
+                    <i class="em em-arrow_down"></i>
+                </p>
+                <a id="codersWeb" href="http://adamjpett.com//">
+                    <button type="button" class="myButton">
+                        Adam's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
+            <div id="floatright" class="col-lg-4 col-md-6 col-sm-6">
+                <img id="photos" src="http://tinyurl.com/hatgfm6" class="img-circle">
+                <p id="codingCoders"><strong> Danyel Johnson </strong>
+                    <br>
+                <p>
+                    <br>
+                    <i class="em em-arrow_down"></i>
+                    <i class="em em-pizza"></i>
+                    <i class="em em-arrow_down"></i>
+                </p>
+                <a id="codersWeb" href="http://danyelmarie.com//">
+                    <button type="button" class="myButton">
+                        Danyel's Web
+                    </button>
+                </a>
+                </p>
+            </div>
+
         </article>
     </section>
 
